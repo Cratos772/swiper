20fb7ec9a388ca30fc499a797d5b91e3f39ad7fb
Cratos772 <77541489+Cratos772@users.noreply.github.com>
2021-01-17 23:14:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var mySwiper = new Swiper('.image-slider', {
+
+  loop: true,
+  touchRatio: 1,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+});
